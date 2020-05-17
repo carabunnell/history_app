20e66812ba52c75813ba3a64e2a055d26fb1855e
Cara Bunnell <carabunnell@gmail.com>
2020-05-17 14:35:28 -0700

    initial commit with passport login

---
+++ public/js/members.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  $.get("/api/user_data").then(function(data) {
+    $(".member-name").text(data.email);
+  });
+
+// untested code for geolocation for the member once they log in. 
+var x = document.getElementById("demo");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+});
